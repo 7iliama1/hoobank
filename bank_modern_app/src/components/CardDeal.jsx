7e921d8ee1c244e9ff1abcb9da222ad68ac926b0
Ruslan Niazbaev <7iliama1@gmail.com>
2024-01-07 02:43:04 +0200

    final build

---
+++ bank_modern_app/src/components/CardDeal.jsx
@@ -0,0 +1,28 @@
+import { card } from "../assets";
+import styles, { layout } from "../style";
+import Button from "./Button";
+
+const CardDeal = () => (
+  <section className={layout.section}>
+    <div className={layout.sectionInfo}>
+      <h2 className={styles.heading2}>
+        Find a better card deal <br className="sm:block hidden" /> in few easy
+        steps.
+      </h2>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+      To snag a superior card deal effortlessly, 
+      first, compare various credit card offers based on rewards, APR, and fees.
+      Next, leverage online tools to locate exclusive sign-up bonuses or promotions. 
+      Lastly, consider contacting your current card issuer to negotiate better terms or perks.
+      </p>
+
+      <Button styles={`mt-10`} />
+    </div>
+
+    <div className={layout.sectionImg}>
+      <img src={card} alt="billing" className="w-[100%] h-[100%]" />
+    </div>
+  </section>
+);
+
+export default CardDeal;
